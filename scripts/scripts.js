569718c08ab028897e84cbd4f78317ac66d8cc32
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-06-28 12:49:15 -0400

    Added hamburger toggle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,18 @@
+
+const app = {};
+
+// Hamburger Menu toggle
+app.toggleNav = () => {
+	$('.hamburger').on('click', function() {
+
+		$('.header__nav').toggleClass('hamburger__visible');
+	});
+} 
+
+// DOCUMENT READY
+
 $(function(){
-
-$('.hamburger').on('click', );
-
-
-
+	console.log('hello world');
+	app.toggleNav();
 
 }); 
